a3c1e0b9f4414d256de9a9d7ebc7619f3ab34f16
mscalfi <m.scalfi97@gmail.com>
2018-06-06 11:15:57 +0200

    VERDE: testRubberDuck e testGoose passano

--- src/main/java/it/unimi/di/se/lab06/Goose.java
+++ src/main/java/it/unimi/di/se/lab06/Goose.java
@@ -3,5 +3,6 @@ package it.unimi.di.se.lab06;
 public class Goose {
 
     public void honk() {
+        System.out.println("honk");
     }
 }
--- src/main/java/it/unimi/di/se/lab06/RubberDuck.java
+++ src/main/java/it/unimi/di/se/lab06/RubberDuck.java
@@ -3,5 +3,6 @@ package it.unimi.di.se.lab06;
 public class RubberDuck {
 
     public void squeak() {
+        System.out.println("squeak");
     }
 }
